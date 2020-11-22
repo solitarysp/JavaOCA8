7addf6099096325128de6dacf2009f98916f217b
solit <lethanh9398@gmail.com>
2020-11-22 23:14:53 +0700

    init

---
+++ Chap1JavaBuildingBlocks/src/main/java/com/lethanh98/java/learn/oca8/chap1/test/q7/aquarium/jellies/Water.java
@@ -0,0 +1,4 @@
+package com.lethanh98.java.learn.oca8.chap1.test.q7.aquarium.jellies;
+
+public class Water {
+}
---
+++ Chap1JavaBuildingBlocks/src/main/java/com/lethanh98/java/learn/oca8/chap1/test/q7/employee/WaterFiller.java
@@ -0,0 +1,10 @@
+package com.lethanh98.java.learn.oca8.chap1.test.q7.employee;
+//A đúng
+//import com.lethanh98.java.learn.oca8.chap1.test.q7.aquarium.*;
+
+import com.lethanh98.java.learn.oca8.chap1.test.q7.aquarium.Water;
+import com.lethanh98.java.learn.oca8.chap1.test.q7.aquarium.jellies.*;
+
+public class WaterFiller {
+    Water water;
+}
