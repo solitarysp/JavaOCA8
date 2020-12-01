86dd92d1af9a4f8cab2acf829d2ec074a3dbec82
solit <lethanh9398@gmail.com>
2020-12-01 23:02:40 +0700

    init

--- Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/UnderstandingJavaOperators/binary/AssignmentOperators.java
+++ Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/UnderstandingJavaOperators/binary/AssignmentOperators.java
@@ -5,6 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
+/**
+ * Một assignment operator là một Binary Operator được sử dụng để Assignment (gán) dữ liệu cho variable ở bên trái toán tử,
+ * với data được gán là giá trị  bên phải của trương trình.
+ * Chúng ta không thể gắn 1 giá trị vượt quá giá trị có thể lưu của variable
+ */
 public class AssignmentOperators implements Operators {
     public static void main(String[] args) {
         Operators operators = new AssignmentOperators();
@@ -14,19 +19,24 @@ public class AssignmentOperators implements Operators {
     @Override
     public void run() {
         example1();
+        example2();
     }
 
     private void example1() {
-        int x = 3;
-        int y = ++x * 5 / x-- + --x;
-        /**
-         * 4 * 5 / x-- + --x
-         * 20 / 4 + --x  ( vì x-- nên sau khi hết biểu thức này, x sẽ =3 )
-         * 5 + 2  ( --x thì = --3 nên x = 2)
-         * 7
-         */
-        log.info("x is " + x);
-        log.info("y is " + y);
+        int x = 3; // Câu lệnh chỉ định với x được gán vault là 3
+    }
+
+    /**
+     * Khi chúng ta sử dụng assignment, java có thể tự động chuyển dữ liệu nhỏ hơn sang lớn hớn. . Int -> long
+     * Nhưng sẽ có 1 lỗi error nếu chúng ta cố gắng chuyển đổi một kiểu dữ liệu lớn hơn sang nhỏ hơn. long < int
+     */
+    private void example2() {
+        int a = 30000000;
+        long b = a; // OK bình thường vì từ nhỏ đến lớn.
+//        int c = b; // Lỗi compiler vì không thể từ lớn đến nhỏ
+//        int d = 1.2; // Lỗi compiler vì không thể từ lớn đến nhỏ
+//        int f = 1f; // Lỗi compiler vì không thể từ lớn đến nhỏ
+        //  short y = 1921222;// không thể vì giá trị vượt quá data type
     }
 
 
---
+++ Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/UnderstandingJavaOperators/binary/CastingPrimitiveValues.java
@@ -0,0 +1,52 @@
+package com.lethanh98.java.learn.oca8.chap2.UnderstandingJavaOperators.binary;
+
+import com.lethanh98.java.learn.oca8.chap2.Operators;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+/**
+ * https://docs.oracle.com/javase/specs/jls/se8/html/jls-5.html#jls-5.1.3
+ */
+public class CastingPrimitiveValues implements Operators {
+    public static void main(String[] args) {
+        Operators operators = new CastingPrimitiveValues();
+        operators.run();
+    }
+
+    @Override
+    public void run() {
+        example1();
+        OverflowAndUnderflow();
+    }
+
+    private void OverflowAndUnderflow() {
+        /**
+         * Một Overflow xẩy ra khi giá trị chúng ta casting, + data...etc..
+         * quá lớn só với dung lượng của data type nó khiến dữ liệu không còn năm trong datatype nữa
+         */
+        short a = (short) 1921222;  // Stored as 20678
+        log.info("a: {}", a);
+
+        /**
+         * Một Underflow xẩy ra Khi 1 giá trị quá nhỏ só với data có thể lưu được
+         */
+        short b = (short) -1921222;  // Stored as -20678
+
+        log.info("b: {}", b);
+        log.info("=====================End example2 =====================");
+
+    }
+
+    private void example1() {
+        int x = (int) 1.0;
+        short y = (short) 1921222;  // Stored as 20678
+        int z = (int) 9l;
+        long t = 192301398193810323L;
+        log.info("int x {} , short y {} , z {}, t {}", x, y, z, t);
+        log.info("=====================End example1 =====================");
+
+    }
+
+
+}
