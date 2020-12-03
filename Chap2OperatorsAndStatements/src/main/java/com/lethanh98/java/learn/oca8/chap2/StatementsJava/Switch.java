00fbc313f88602f98f4b4eb6fc8523ae6bb17c0d
solit <lethanh9398@gmail.com>
2020-12-03 22:26:14 +0700

    init

--- Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/StatementsJava/Switch.java
+++ Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/StatementsJava/Switch.java
@@ -14,72 +14,90 @@ public class Switch implements Operators {
 
     @Override
     public void run() {
-        example1(5);
-        example2(5);
+        example1();
+        example3();
+        example5();
     }
 
-    private void example1(int number) {
+    private void example1() {
         log.info("==================example1============");
-        switch (getData(number)) {
+        switch (5) {
+            case 1: {
+                log.info("1");
+                break;
+            }
             default: {
                 log.info("Default");
             }
-            case 1: {
-                log.info("1");
-                break;
-            }
-
-        }
-    }
-
-    private void example2(int number) {
-        log.info("==================example2============");
-
-        switch (getData(number)) {
-            case 1:
-                String result="1";
-                log.info("1");
-                break;
-            case 2:
+            case 2: {
                 log.info("2");
-                break;
-
+            }
         }
     }
 
-    private void example3(int number) {
+//    private void example2() {
+//        log.info("==================example2============");
+//        switch (5) {
+//            case 1:
+//                String result = "1";
+//                log.info("1");
+//                break;
+//            case 2:
+//                String result = "1";
+//                log.info("2");
+//                break;
+//        }
+//    }
+
+    private void example3() {
         log.info("==================example3============");
-        switch (getData(number)) {
+        switch (getData(1)) {
             case 1: {
-                String result="1";
+                String result = "1";
                 log.info("1");
                 break;
             }
             case 2: {
-                String result="1";
+                String result = "1";
                 log.info("2");
                 break;
             }
 
         }
     }
-  /*  private void example4(int number) {
-        log.info("==================example4============");
-        int dataForCase= 1;
-        switch (getData(number)) {
-            case 1: {
-                String result="1";
+
+//    private void example4(int number) {
+//        log.info("==================example4============");
+//        int dataForCase= 1;
+//        switch (getData(number)) {
+//            case 1: {
+//                String result="1";
+//                log.info("1");
+//                break;
+//            }
+//            case dataForCase: {
+//                String result="1";
+//                log.info("2");
+//                break;
+//            }
+//
+//        }
+//    }
+
+    private void example5() {
+        log.info("==================example5============");
+        switch (1) {
+            case '1': {
                 log.info("1");
                 break;
             }
-            case dataForCase: {
-                String result="1";
+            case 2: {
                 log.info("2");
                 break;
             }
 
         }
-    }*/
+    }
 
     private int getData(int number) {
         return number;
--- Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/StatementsJava/Ternary.java
+++ Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/StatementsJava/Ternary.java
@@ -16,21 +16,41 @@ public class Ternary implements Operators {
     public void run() {
         example1(9);
         example2();
+        example3();
+        example4();
     }
 
-    private void example2() {
-        int number=1;
-        boolean result = cal(number++) & cal(number++) ? true : false;
-        log.info("{}, and {}", result,number);
-    }
-
-    private boolean cal(int number) {
-        return number>1;
+    private void example4() {
+        log.info("==================example4============");
+        int number = 1;
+        float a = number > 1 ? 1F : 2L;
     }
 
     private void example1(int number) {
-        log.info("{}", (number > 5) ? 21 : "Zebra");
-
+        log.info("==================example1============");
+        int result1 = 1;
+        Long result = (number > 5) ? result1 : 10L;
     }
+
+    private void example2() {
+        log.info("==================example2============");
+        int number = 1;
+        boolean result = cal(number++) & cal(number++) ? true : false;
+        log.info("{}, and {}", result, number);
+    }
+
+    private boolean cal(int number) {
+        return number > 1;
+    }
+
+    private void example3() {
+        log.info("==================example3============");
+        int number = 1;
+        char a = number > 1 ? '1' : 255;
+        //  char b = number > 1 ? 9 : 255;
+        log.info("{}", a);
+        // log.info("{}",b);
+    }
+
 
 }
