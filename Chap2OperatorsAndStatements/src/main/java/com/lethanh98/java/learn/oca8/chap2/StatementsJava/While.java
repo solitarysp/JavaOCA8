68979039dc1bb361ed5208e3a2ed1ba61da8c912
solit <lethanh9398@gmail.com>
2020-12-03 22:45:32 +0700

    init

---
+++ Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/StatementsJava/While.java
@@ -0,0 +1,37 @@
+package com.lethanh98.java.learn.oca8.chap2.StatementsJava;
+
+import com.lethanh98.java.learn.oca8.chap2.Operators;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+
+public class While implements Operators {
+    public static void main(String[] args) {
+        Operators operators = new While();
+        operators.run();
+    }
+
+    @Override
+    public void run() {
+        example1();
+        exampleInfiniteLoops();
+    }
+
+    private void exampleInfiniteLoops() {
+        while (true) {
+            int a = 1;
+        }
+    }
+
+    // ví dụ không bao giờ chạy block vì lần đầu đã false
+    private void example1() {
+        log.info("=====================example1==================");
+        int a = 1;
+        while (a > 1) {
+            log.info("A > 1");
+        }
+    }
+
+
+}
