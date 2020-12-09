a0c6fa0567e9a80be736ecf6eefb73d6b9ffbbb4
lethanh9398 <lethanh9398@gmail.com>
2020-12-09 17:34:53 +0700

    add

--- Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/StatementsJava/AdvancedFlowControl/Break.java
+++ Chap2OperatorsAndStatements/src/main/java/com/lethanh98/java/learn/oca8/chap2/StatementsJava/AdvancedFlowControl/Break.java
@@ -18,16 +18,19 @@ public class Break implements Operators {
     public void run() {
         example1();
         example2();
+        exampleUsingLabelBreak();
+        exampleWhileLoop();
     }
-    // Nêu câu lệnh break không có label, bread sẽ làm kết thúc vòng lặp gần nhất
-    private void example2() {
-        log.info("==========================example2=====================");
-        for (int i = 0; i < 5; i++) {
-            log.info("i {}", i);
-            for (int j = 0; j < 10000; j++) {
-                log.info("j {}",j);
+
+    private void exampleWhileLoop() {
+        log.info("==========================exampleWhileLoop=====================");
+        int len = 0;
+        while (len < 6) {
+            log.info("check len {}",len);
+            if (len == 3) {
                 break;
             }
+            len++;
         }
     }
 
@@ -40,6 +43,36 @@ public class Break implements Operators {
                 break;
             }
         }
+    }
+    // Nêu câu lệnh break không có label, bread sẽ làm kết thúc vòng lặp gần nhất
+    private void example2() {
+        log.info("==========================example2=====================");
+        for (int i = 0; i < 5; i++) {
+            log.info("i {}", i);
+            for (int j = 0; j < 10000; j++) {
+                log.info("j {}", j);
+                break;
+            }
+        }
+    }
+    // Sử dụng label chúng ta có thể kết thúc vòng lặp level cao hơn ví dụ cha, hoặc cha của cha...etc..
+    private void exampleUsingLabelBreak() {
+        log.info("==========================exampleUsingLabelBreak=====================");
+        int idFind = 6;
+        int[][] list = {{1, 2, 3}, {4, 5, 6}, {6, 8, 9}};
+        int index = -1;
+
+PARENT_LOOP:
+        for (int i = 0; i < list.length; i++) {
+            log.info("i= {}", i);
+            for (int j = 0; j < list[i].length; j++) {
+                if (list[i][j] == idFind) {
+                    index = i;
+                    break PARENT_LOOP;
+                }
+            }
+        }
+        log.info("index {}", index);
     }
 
 }
