4ee9b9525d1591951a3ce511e87e3813749ae24a
lethanh9398 <lethanh9398@gmail.com>
2021-01-13 10:41:20 +0700

    add

---
+++ Chap5ClassDesign/src/main/java/com/lethanh98/java/learn/oca8/chap5/ConstructorsInheritance/ConstructorsInheritance.java
@@ -0,0 +1,50 @@
+package com.lethanh98.java.learn.oca8.chap5.ConstructorsInheritance;
+
+import lombok.Data;
+
+public class ConstructorsInheritance {
+    public static void main(String[] args) {
+        StudentPro studentPro=new StudentPro("thanh");
+    }
+}
+
+@Data
+class Student {
+    private String name;
+    private Integer age;
+    public Student(String name) {
+        this(name, 18);
+        System.out.println("Student(String name)");
+    }
+
+    public Student(String name, Integer age) {
+        super();
+        this.name = name;
+        this.age = age;
+        System.out.println("Student(String name, Integer age)");
+    }
+}
+
+class StudentVip extends Student {
+
+    public StudentVip(String name) {
+        super(name);
+     //   this(name);
+        System.out.println("StudentVip(String name)");
+    }
+    public StudentVip(String name, Integer age) {
+        super(name, age);
+        System.out.println("StudentVip(String name, Integer age)");
+    }
+}
+class StudentPro extends StudentVip {
+    public StudentPro(String name) {
+        super(name);
+        System.out.println("StudentPro(String name)");
+    }
+    public StudentPro(String name, Integer age) {
+        super(name, age);
+        System.out.println("StudentPro(String name, Integer age)");
+
+    }
+}
---
+++ Chap5ClassDesign/src/main/java/com/lethanh98/java/learn/oca8/chap5/extendjavaobject/thongthuong/ClassDemo.java
@@ -0,0 +1,19 @@
+package com.lethanh98.java.learn.oca8.chap5.extendjavaobject.thongthuong;
+
+public class ClassDemo {
+
+}
+
+class A extends java.lang.Object {
+
+}
+
+class B extends A {
+
+}
+
+class C extends B {
+
+}
+
+
