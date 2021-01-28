a378ba299a31994942807f13472d96e03853c143
solit <s>
2021-01-28 22:41:50 +0700

    add

---
+++ Chap6Exceptions/src/main/java/com/lethanh98/java/learn/oca8/chap6/BaseInterface.java
@@ -0,0 +1,6 @@
+package com.lethanh98.java.learn.oca8.chap6;
+
+public interface BaseInterface {
+    public String BASE_LOG = "==================================={}===================================";
+    public abstract   void run() throws Exception;
+}
---
+++ Chap6Exceptions/src/main/java/com/lethanh98/java/learn/oca8/chap6/Error/ErrorSimple.java
@@ -0,0 +1,43 @@
+package com.lethanh98.java.learn.oca8.chap6.Error;
+
+public class ErrorSimple {
+
+}
+
+class Test1 {
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        throw new Error();
+    }
+}
+
+class Test2 {
+    public static void main(String[] args) {
+        test();
+    }
+
+    // Error cũng là 1 dạng runtime exeption  (unchekced)
+    // Các lỗi error sẽ khong bắt buộc runtime phải hanling
+    // Các Error chúng ta cũng không nên hanling
+    private static void test() {
+        throw new Error();
+    }
+}
+
+class Test3 {
+    public static void main(String[] args) {
+        try {
+            test();
+            // chúng ta vẫn có thể handling error
+        } catch (Error error) {
+            System.out.printf("Error");
+        }
+    }
+
+    private static void test() {
+        throw new Error();
+    }
+}
