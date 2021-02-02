f40e973a9f55553b6496ff986b56fe9a3ab8913c
solitarysp <solitarysp@gmail.com>
2021-02-02 08:46:39 +0700

    add

--- Chap6Exceptions/src/main/java/com/lethanh98/java/learn/oca8/chap6/ForgetCatchException/ForgetCatchExceptionSimple.java
+++ Chap6Exceptions/src/main/java/com/lethanh98/java/learn/oca8/chap6/ForgetCatchException/ForgetCatchExceptionSimple.java
@@ -3,14 +3,16 @@ package com.lethanh98.java.learn.oca8.chap6.ForgetCatchException;
 public class ForgetCatchExceptionSimple {
     public static void main(String[] args) {
         try {
-            int a = 1;
-            int c = a / 0;
             test();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
+    /**
+     * Nếu có Exception trong finally  thì Exception trong catch sẽ được bỏ qua.
+     * @throws Exception
+     */
     private static void test() throws Exception {
         try {
             throw new RuntimeException("Error in try");
